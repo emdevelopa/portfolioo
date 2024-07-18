e3b6ef21d53dc9c4e52359f76cf384dbd0ce77d4
emdevelopa <gyimahsunday@gmail.com>
2024-07-18 15:49:53 +0100

    niceone

--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -9,12 +9,12 @@ const Navbar = () => {
   return (
     <section className="fixed bottom-4 w-full flex justify-center z-10">
       <nav className="bg-[#171717] w-full sm:w-[80%] md:w-[60%] lg:w-[50%] xl:w-[40%] px-4 py-2 md:px-8 md:py-4 rounded-full">
-        <ul className="flex justify-between text-sm md:text-base">
+        <ul className="flex justify-between text-sm md:text-base text-[#ABB2BF]">
           <li>
             <a
               href="/"
               className={`group hover:text-[#C778DD] hover:underline ${
-                isActive("/") ? "font-bold" : ""
+                isActive("/") ? "font-bold text-white" : ""
               }`}
             >
               <span className="text-[#C778DD] group-hover:text-[#fff] ">#</span>
@@ -25,7 +25,7 @@ const Navbar = () => {
             <a
               href="/projects"
               className={`group hover:text-[#C778DD] hover:underline ${
-                isActive("/projects") ? "font-bold" : ""
+                isActive("/projects") ? "font-bold text-white" : ""
               }`}
             >
               <span className="text-[#C778DD] group-hover:text-[#fff] ">#</span>
@@ -36,7 +36,7 @@ const Navbar = () => {
             <a
               href="/aboutme"
               className={`group hover:text-[#C778DD] hover:underline ${
-                isActive("/aboutme") ? "font-bold" : ""
+                isActive("/aboutme") ? "font-bold text-white" : ""
               }`}
             >
               <span className="text-[#C778DD] group-hover:text-[#fff] ">#</span>
@@ -47,7 +47,7 @@ const Navbar = () => {
             <a
               href="/contacts"
               className={`group hover:text-[#C778DD] hover:underline ${
-                isActive("/contacts") ? "font-bold" : ""
+                isActive("/contacts") ? "font-bold text-white" : ""
               }`}
             >
               <span className="text-[#C778DD] group-hover:text-[#fff] ">#</span>
