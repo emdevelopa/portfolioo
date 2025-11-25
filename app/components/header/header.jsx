904d7454d8e92aeeb983ed58b1120672dd758c1a
SOLO-DEV <111129738+emdevelopa@users.noreply.github.com>
2025-11-25 22:57:41 +0100

    Update header.jsx

--- app/components/header/header.jsx
+++ app/components/header/header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <section className="text-center pt-[2em]">
-      <h1 className="font-bold text-[30px]">EM-DEV</h1>
+      <h1 className="font-bold text-[30px]">SOLO_DEV#999🏀💻🎧</h1>
     </section>
   );
 };
